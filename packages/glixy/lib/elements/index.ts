19c9d952b02be6f4a930a241bb36b8b2c10892ee
Szymeo <dev.sgracki@gmail.com>
2024-12-27 02:07:49 +0100

    wip: quick commit

--- packages/glixy/lib/elements/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Stage } from './Stage.svelte';
-export { default as Container } from './Container.svelte';
-export { default as Sprite } from './Sprite.svelte';
--- packages/glixy/lib/utils/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './is-defined.js';
